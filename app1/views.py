c6111cf3559c607d07c28f79c7172ddb1ecd7eb1
nikhil <nikhilgajraj07@gmail.com>
2025-08-31 19:23:41 +0530

    improve pages

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest , HttpResponse
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login
